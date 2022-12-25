f9dc0bc9bf65c79b36c91bf1c987fab5fbebe2ff
PK4US <stepanchuk.ei@gmail.com>
2022-12-26 01:21:02 +0200

    Buttons in Jetpack Compose

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,41 +3,140 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.pk4us.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SimpleButton() {
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Simple Button")
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
+fun ButtonWithColor() {
+    Button(
+        onClick = {
+            //your onclick code
+        }, colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray)
+    )
+
+    {
+        Text(text = "Button with gray background", color = Color.White)
+    }
+}
+
+
+@Preview
+@Composable
+fun ButtonWithTwoTextView() {
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Click ", color = Color.Magenta)
+        Text(text = "Here", color = Color.Green)
+    }
+}
+
+
+@Preview
+@Composable
+fun ButtonWithIcon() {
+    Button(onClick = {}) {
+        Image(
+            painterResource(id = R.drawable.ic_cart),
+            contentDescription = "Cart button icon",
+            modifier = Modifier.size(20.dp)
+        )
+
+        Text(text = "Add to cart", Modifier.padding(start = 10.dp))
+    }
+}
+
+
+@Preview
+@Composable
+fun ButtonWithRectangleShape() {
+    Button(onClick = {}, shape = RectangleShape) {
+        Text(text = "Rectangle shape")
+    }
+}
+
+
+@Preview
+@Composable
+fun ButtonWithRoundCornerShape() {
+    Button(onClick = {}, shape = RoundedCornerShape(20.dp)) {
+        Text(text = "Round corner shape")
+    }
+}
+
+
+@Preview
+@Composable
+fun ButtonWithCutCornerShape() {
+    //CutCornerShape(percent: Int)- it will consider as percentage
+    //CutCornerShape(size: Dp)- you can pass Dp also.
+    //Here we use Int, so it will take percentage.
+    Button(onClick = {}, shape = CutCornerShape(15)) {
+        Text(text = "Cut corner shape")
+    }
+}
+
+
+@Preview
+@Composable
+fun ButtonWithBorder() {
+    Button(
+        onClick = {
+            //your onclick code
+        },
+        border = BorderStroke(1.dp, Color.Red),
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
+    ) {
+        Text(text = "Button with border", color = Color.DarkGray)
+    }
+}
+
+
+@Preview
+@Composable
+fun ButtonWithElevation() {
+    Button(
+        onClick = {
+            //your onclick code here
+        }, elevation = ButtonDefaults.elevation(
+            defaultElevation = 10.dp, pressedElevation = 15.dp, disabledElevation = 0.dp
+        )
+    ) {
+        Text(text = "Button with elevation")
     }
 }

44cbfefc58cadf70513873e3bf657c8e768b44b1
PK4US <stepanchuk.ei@gmail.com>
2022-12-26 22:33:58 +0200

    TextField in Jetpack Compose

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,41 +3,129 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import com.pk4us.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            TextFieldAll()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StyledTextField() {
+    var value by remember { mutableStateOf("Hello\nWorld\nInvisible") }
+    TextField(
+        value = value,
+        onValueChange = { value = it },
+        label = { Text("Enter text") },
+        maxLines = 2,
+        textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+        modifier = Modifier.padding(20.dp)
+    )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
+fun SimpleTextField() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(value = text,
+        modifier = Modifier.padding(20.dp),
+        onValueChange = { newText ->
+            text = newText
+        })
+}
+
+
+@Preview
+@Composable
+fun LabelAndPlaceHolder() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        modifier = Modifier.padding(20.dp),
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Your Label") },
+        placeholder = { Text(text = "Your Placeholder/Hint") },
+    )
+}
+
+
+@Preview
+@Composable
+fun TextFieldWithInputType() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(value = text,
+        modifier = Modifier.padding(20.dp),
+        label = { Text(text = "Number Input Type") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { it ->
+            text = it
+        })
+}
+
+
+@Preview
+@Composable
+fun OutLineTextFieldSample() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(value = text,
+        modifier = Modifier.padding(20.dp),
+        label = { Text(text = "Enter Your Name") },
+        onValueChange = {
+            text = it
+        })
+}
+
+
+@Preview
+@Composable
+fun TextFieldWithIcons() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    return OutlinedTextField(
+        value = text,
+        modifier = Modifier.padding(20.dp),
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
+        //trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Email address") },
+        placeholder = { Text(text = "Enter your e-mail") },
+    )
+}
+
+@Preview
+@Composable
+fun TextFieldAll() {
+    Column() {
+        StyledTextField()
+        SimpleTextField()
+        LabelAndPlaceHolder()
+        TextFieldWithInputType()
+        OutLineTextFieldSample()
+        TextFieldWithIcons()
     }
 }

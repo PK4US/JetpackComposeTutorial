e7d4419fa13c3002941dc03b6bf9de1cb7d1c213
PK4US <stepanchuk.ei@gmail.com>
2022-12-26 02:04:31 +0200

    Image in JetPack Compose

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,41 +3,111 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.pk4us.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
+
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.andy_rubin),
+        contentDescription = "Andy Rubin",
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
-    }
+fun CircleImageView() {
+    Image(
+        painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Circle Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(CircleShape) // clip to the circle shape
+            .border(5.dp, Color.Gray, CircleShape)//optional
+    )
+}
+
+
+@Preview
+@Composable
+fun RoundCornerImageView() {
+    Image(
+        painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Round corner image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(RoundedCornerShape(10))
+            .border(5.dp, Color.Gray, RoundedCornerShape(10))
+    )
+}
+
+
+@Preview
+@Composable
+fun ImageWithBackgroundColor() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_cart),
+            contentDescription = "",
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.DarkGray)
+            .padding(20.dp)
+    )
+}
+
+
+@Preview
+@Composable
+fun ImageWithTint() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_cart),
+        contentDescription = "",
+        colorFilter = ColorFilter.tint(Color.Red),
+        modifier = Modifier
+            .size(200.dp)
+    )
+}
+
+
+@Preview
+@Composable
+fun InsideFit() {
+    Image(
+        painter = painterResource(id = R.drawable.andy_rubin),
+        contentDescription = "",
+        modifier = Modifier
+            .size(150.dp)
+            .background(Color.LightGray),
+        contentScale = ContentScale.Inside
+    )
 }

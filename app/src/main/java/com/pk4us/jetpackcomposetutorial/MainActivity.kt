974908aded5ef5209a75f9bd40779bfa44440498
PK4US <stepanchuk.ei@gmail.com>
2023-01-04 00:10:41 +0200

    Jetpack Compose Navigation Drawer with Scaffold

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -1,43 +1,317 @@
 package com.pk4us.jetpackcomposetutorial
 
+
+
+
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.pk4us.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+                    val scaffoldState = rememberScaffoldState()
+                    val coroutineScope = rememberCoroutineScope()
+                    val contextForToast = LocalContext.current.applicationContext
+
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        scaffoldState = scaffoldState,
+                        topBar = {
+                            MyTopAppBar {
+                                coroutineScope.launch {
+                                    scaffoldState.drawerState.open()
+                                }
+                            }
+                        },
+                        drawerContent = {
+                            DrawerContent { itemLabel ->
+                                Toast
+                                    .makeText(contextForToast, itemLabel, Toast.LENGTH_SHORT)
+                                    .show()
+                                coroutineScope.launch {
+                                    // delay for the ripple effect
+                                    delay(timeMillis = 250)
+                                    scaffoldState.drawerState.close()
+                                }
+                            }
+                        }
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "Rest of the App UI Here")
+                        }
+                    }
                 }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyTopAppBar(onNavIconClick: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = "SemicolonSpace") },
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    onNavIconClick()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Open Navigation Drawer"
+                )
+            }
+        }
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
+private fun DrawerContent(
+    gradientColors: List<Color> = listOf(Color(0xFFF70A74), Color(0xFFF59118)),
+    itemClick: (String) -> Unit
+) {
+
+    val itemsList = prepareNavigationDrawerItems()
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(colors = gradientColors)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(vertical = 36.dp)
+    ) {
+
+        item {
+
+            // user's image
+            Image(
+                modifier = Modifier
+                    .size(size = 120.dp)
+                    .clip(shape = CircleShape),
+                painter = painterResource(id = R.drawable.people1),
+                contentDescription = "Profile Image"
+            )
+
+            // user's name
+            Text(
+                modifier = Modifier
+                    .padding(top = 12.dp),
+                text = "Hermione",
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+
+            // user's email
+            Text(
+                modifier = Modifier.padding(top = 8.dp, bottom = 30.dp),
+                text = "hermione@email.com",
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+
+        items(itemsList) { item ->
+            NavigationListItem(item = item) {
+                itemClick(item.label)
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun NavigationListItem(
+    item: NavigationDrawerItem,
+    unreadBubbleColor: Color = Color(0xFF0FFF93),
+    itemClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                itemClick()
+            }
+            .padding(horizontal = 24.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        // icon and unread bubble
+        Box {
+
+            Icon(
+                modifier = Modifier
+                    .padding(all = if (item.showUnreadBubble && item.label == "Messages") 5.dp else 2.dp)
+                    .size(size = if (item.showUnreadBubble && item.label == "Messages") 24.dp else 28.dp),
+                painter = item.image,
+                contentDescription = null,
+                tint = Color.White
+            )
+
+            // unread bubble
+            if (item.showUnreadBubble) {
+                Box(
+                    modifier = Modifier
+                        .size(size = 8.dp)
+                        .align(alignment = Alignment.TopEnd)
+                        .background(color = unreadBubbleColor, shape = CircleShape)
+                )
+            }
+        }
+
+        // label
+        Text(
+            modifier = Modifier.padding(start = 16.dp),
+            text = item.label,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+private fun prepareNavigationDrawerItems(): List<NavigationDrawerItem> {
+    val itemsList = arrayListOf<NavigationDrawerItem>()
+
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.home),
+            label = "Home"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.message_square),
+            label = "Messages",
+            showUnreadBubble = true
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.bell),
+            label = "Notifications",
+            showUnreadBubble = true
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.user),
+            label = "Profile"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.credit_card),
+            label = "Payments"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.settings),
+            label = "Settings"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.log_out),
+            label = "Logout"
+        )
+    )
+
+    return itemsList
+}
+
+data class NavigationDrawerItem(
+    val image: Painter,
+    val label: String,
+    val showUnreadBubble: Boolean = false
+)
+
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Preview
+@Composable
+fun ScaffoldWithTopBar() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val selectedItem = remember { mutableStateOf("") }
+    val items = listOf("Главная", "Контакты", "О приложении")
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            for (item in items) {
+                Text(
+                    item,
+                    fontSize = 28.sp,
+                    modifier = Modifier.clickable {
+                        selectedItem.value = item
+                        scope.launch { scaffoldState.drawerState.close() }
+                    }
+                )
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Top App Bar") },
+                    navigationIcon = {
+                        IconButton(onClick = { scope.launch { scaffoldState.drawerState.open() } }) {
+                            Icon(
+                                Icons.Filled.Menu,
+                                "backIcon"
+                            )
+                        }
+                    }
+                )
+            },
+            content = {
+                Text(selectedItem.value, fontSize = 28.sp)
+            })
     }
 }

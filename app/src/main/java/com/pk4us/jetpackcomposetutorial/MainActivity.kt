46f543a6d1f7543201dd014a8e2c7f0cbeecaa8a
PK4US <stepanchuk.ei@gmail.com>
2023-01-06 14:28:06 +0200

    Custom Switch 1,2,3

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -1,14 +1,273 @@
 package com.pk4us.jetpackcomposetutorial
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.pk4us.jetpackcomposetutorial.ui.theme.Purple700
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.DarkGray),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Switch1()
+                    Spacer(modifier = Modifier.height(18.dp))
+                    Switch2()
+                    Spacer(modifier = Modifier.height(18.dp))
+                    Switch3()
+                    Spacer(modifier = Modifier.height(18.dp))
+                    CustomSwitch()
+                }
+            }
         }
     }
+}
+
+@Composable
+fun Switch1() {
+    val switchON = remember { mutableStateOf(true) }
+    Switch(checked = switchON.value, onCheckedChange = {switchON.value = it})
+    Text(text = if (switchON.value) "ON" else "OFF")
+}
+
+
+@Composable
+fun Switch2(
+    scale: Float = 2f,
+    width: Dp = 36.dp,
+    height: Dp = 20.dp,
+    checkedTrackColor: Color = Color(0xFF00FF0A),
+    uncheckedTrackColor: Color = Color(0xFFFF0000),
+    thumbColor: Color = Color.White,
+    gapBetweenThumbAndTrackEdge: Dp = 4.dp
+) {
+
+    val switchON = remember { mutableStateOf(true) }
+    val thumbRadius = (height / 2) - gapBetweenThumbAndTrackEdge
+    // To move the thumb, we need to calculate the position (along x axis)
+    val animatePosition = animateFloatAsState(
+        targetValue = if (switchON.value)
+            with(LocalDensity.current) { (width - thumbRadius - gapBetweenThumbAndTrackEdge).toPx() }
+        else
+            with(LocalDensity.current) { (thumbRadius + gapBetweenThumbAndTrackEdge).toPx() }
+    )
+
+    Canvas(
+        modifier = Modifier
+            .size(width = width, height = height)
+            .scale(scale = scale)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        // This is called when the user taps on the canvas
+                        switchON.value = !switchON.value
+                    }
+                )
+            }
+    ) {
+
+        // Track
+        drawRoundRect(
+            color = if (switchON.value) checkedTrackColor else uncheckedTrackColor,
+            cornerRadius = CornerRadius(x = 10.dp.toPx(), y = 10.dp.toPx())
+        )
+
+        // Thumb
+        drawCircle(
+            color = thumbColor,
+            radius = thumbRadius.toPx(),
+            center = Offset(
+                x = animatePosition.value,
+                y = size.height / 2
+            )
+        )
+
+    }
+
+    Spacer(modifier = Modifier.height(18.dp))
+
+    Text(text = if (switchON.value) "ON" else "OFF")
+}
+
+@Composable
+fun Switch3(
+    scale: Float = 2f,
+    width: Dp = 36.dp,
+    height: Dp = 20.dp,
+    strokeWidth: Dp = 2.dp,
+    checkedTrackColor: Color = Color(0xFF00FF0A),
+    uncheckedTrackColor: Color = Color(0xFFFF0000),
+    gapBetweenThumbAndTrackEdge: Dp = 4.dp
+) {
+
+    val switchON = remember {
+        mutableStateOf(true) // Initially the switch is ON
+    }
+
+    val thumbRadius = (height / 2) - gapBetweenThumbAndTrackEdge
+
+    // To move thumb, we need to calculate the position (along x axis)
+    val animatePosition = animateFloatAsState(
+        targetValue = if (switchON.value)
+            with(LocalDensity.current) { (width - thumbRadius - gapBetweenThumbAndTrackEdge).toPx() }
+        else
+            with(LocalDensity.current) { (thumbRadius + gapBetweenThumbAndTrackEdge).toPx() }
+    )
+
+    Canvas(
+        modifier = Modifier
+            .size(width = width, height = height)
+            .scale(scale = scale)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        // This is called when the user taps on the canvas
+                        switchON.value = !switchON.value
+                    }
+                )
+            }
+    ) {
+        // Track
+        drawRoundRect(
+            color = if (switchON.value) checkedTrackColor else uncheckedTrackColor,
+            cornerRadius = CornerRadius(x = 10.dp.toPx(), y = 10.dp.toPx()),
+            style = Stroke(width = strokeWidth.toPx())
+        )
+
+        // Thumb
+        drawCircle(
+            color = if (switchON.value) checkedTrackColor else uncheckedTrackColor,
+            radius = thumbRadius.toPx(),
+            center = Offset(
+                x = animatePosition.value,
+                y = size.height / 2
+            )
+        )
+    }
+
+    Spacer(modifier = Modifier.height(18.dp))
+
+    Text(text = if (switchON.value) "ON" else "OFF")
+}
+
+
+@Composable
+fun CustomSwitch(
+    width: Dp = 72.dp,
+    height: Dp = 40.dp,
+    checkedTrackColor: Color = Color(0xFF00FF0A),
+    uncheckedTrackColor: Color = Color(0xFFFF0000),
+    gapBetweenThumbAndTrackEdge: Dp = 8.dp,
+    borderWidth: Dp = 4.dp,
+    cornerSize: Int = 50,
+    iconInnerPadding: Dp = 4.dp,
+    thumbSize: Dp = 24.dp
+) {
+
+    // this is to disable the ripple effect
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
+    // state of the switch
+    var switchOn by remember {
+        mutableStateOf(true)
+    }
+
+    // for moving the thumb
+    val alignment by animateAlignmentAsState(if (switchOn) 1f else -1f)
+
+    // outer rectangle with border
+    Box(
+        modifier = Modifier
+            .size(width = width, height = height)
+            .border(
+                width = borderWidth,
+                color = if (switchOn) checkedTrackColor else uncheckedTrackColor,
+                shape = RoundedCornerShape(percent = cornerSize)
+            )
+            .clickable(
+                indication = null,
+                interactionSource = interactionSource
+            ) {
+                switchOn = !switchOn
+            },
+        contentAlignment = Alignment.Center
+    ) {
+
+        // this is to add padding at the each horizontal side
+        Box(
+            modifier = Modifier
+                .padding(
+                    start = gapBetweenThumbAndTrackEdge,
+                    end = gapBetweenThumbAndTrackEdge
+                )
+                .fillMaxSize(),
+            contentAlignment = alignment
+        ) {
+
+            // thumb with icon
+            Icon(
+                imageVector = if (switchOn) Icons.Filled.Done else Icons.Filled.Close,
+                contentDescription = if (switchOn) "Enabled" else "Disabled",
+                modifier = Modifier
+                    .size(size = thumbSize)
+                    .background(
+                        color = if (switchOn) checkedTrackColor else uncheckedTrackColor,
+                        shape = CircleShape
+                    )
+                    .padding(all = iconInnerPadding),
+                tint = Color.White
+            )
+        }
+    }
+
+    // gap between switch and the text
+    Spacer(modifier = Modifier.height(height = 16.dp))
+
+    Text(text = if (switchOn) "ON" else "OFF")
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+private fun animateAlignmentAsState(
+    targetBiasValue: Float
+): State<BiasAlignment> {
+    val bias by animateFloatAsState(targetBiasValue)
+    return derivedStateOf { BiasAlignment(horizontalBias = bias, verticalBias = 0f) }
 }

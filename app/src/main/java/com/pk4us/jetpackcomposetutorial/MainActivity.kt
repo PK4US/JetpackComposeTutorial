a9d8a6a47869af6fd9e113bf72716458ffc5d292
PK4US <Stepanchuk.ei@gmail.com>
2023-01-14 14:50:30 +0200

    ----------Material 3 Design ---------

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,12 +3,17 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Material3AppTheme {
 
+            }
         }
     }
 }

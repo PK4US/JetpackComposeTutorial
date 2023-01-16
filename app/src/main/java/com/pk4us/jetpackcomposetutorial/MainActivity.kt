ab9d0b9ef785e197f04eaebb93abd38810c935da
PK4US <Stepanchuk.ei@gmail.com>
2023-01-17 01:22:27 +0200

    3 Индикаторы прогресса

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,7 +3,17 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
@@ -12,8 +22,80 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.background),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    LinearExample()
+                    CircularExample()
+                }
             }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun LinearExample() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        var progress by remember { mutableStateOf(0.1f) }
+        val animatedProgress by animateFloatAsState(
+            targetValue = progress,
+            animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+        )
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .semantics(mergeDescendants = true) {}
+                    .padding(10.dp),
+                progress = animatedProgress,
+            )
+            Spacer(Modifier.requiredHeight(30.dp))
+            OutlinedButton(
+                modifier = Modifier.semantics {
+                    val progressPercent = (progress * 100).toInt()
+                    stateDescription = "Progress $progressPercent%"
+                },
+                onClick = {
+                    if (progress < 1f) progress += 0.1f
+                }
+            ) {
+                Text("Increase")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CircularExample() {
+
+    var progress by remember { mutableStateOf(0.1f) }
+    val animatedProgress by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    )
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        CircularProgressIndicator(progress = animatedProgress)
+        Spacer(Modifier.requiredHeight(30.dp))
+        OutlinedButton(
+            modifier = Modifier.semantics {
+                val progressPercent = (progress * 100).toInt()
+
+                stateDescription = "Progress $progressPercent%"
+
+            },
+            onClick = {
+                if (progress < 1f) progress += 0.1f
+            }
+        ) {
+            Text("Increase")
         }
     }
 }
--- app/src/main/java/com/pk4us/jetpackcomposetutorial/ui/theme/Type.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/ui/theme/Type.kt
@@ -6,11 +6,26 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyMedium = TextStyle(
+    bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     )
 )

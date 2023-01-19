eed24da059be2f52c2ed6954f2075726da2b9203
PK4US <Stepanchuk.ei@gmail.com>
2023-01-20 00:02:02 +0200

    13 Рельс навигации

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,7 +3,13 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
@@ -12,8 +18,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-
+                NavigationRailExample()
             }
+        }
+    }
+}
+
+
+@Preview
+@ExperimentalMaterial3Api
+@Composable
+fun NavigationRailExample() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Home", "Search", "Settings")
+    val icons = listOf(Icons.Filled.Home, Icons.Filled.Search, Icons.Filled.Settings)
+    NavigationRail {
+        items.forEachIndexed { index, item ->
+            NavigationRailItem(
+                icon = { Icon(icons[index], contentDescription = item) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
         }
     }
 }

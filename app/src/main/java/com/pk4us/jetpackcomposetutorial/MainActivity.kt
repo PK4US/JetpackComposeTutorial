b5a7f5885762f9e18cdca4183e3885ef01ef07de
PK4US <Stepanchuk.ei@gmail.com>
2023-01-19 02:16:15 +0200

    10 Нижняя панель приложения

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,7 +3,13 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
@@ -12,8 +18,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-
+                BottomAppBarExample()
             }
         }
     }
+}
+
+@Preview
+@ExperimentalMaterial3Api
+@Composable
+fun BottomAppBarExample() {
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Check, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    Icons.Filled.Edit,
+                    contentDescription = "Localized description",
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* do something */ },
+                containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+            ) {
+                Icon(Icons.Filled.Add, "Localized description")
+            }
+        }
+    )
 }

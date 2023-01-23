b692f58cebffc38e57d0fdc8e186661fb0d5dc33
PK4US <Stepanchuk.ei@gmail.com>
2023-01-23 19:24:18 +0200

    21 Радиокнопка

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,7 +3,18 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
@@ -12,7 +23,66 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
+                Column {
+                    RadioButtonExample1()
+                    Spacer(modifier = Modifier.size(25.dp))
+                    Divider()
+                    Spacer(modifier = Modifier.size(25.dp))
+                    RadioButtonExample2()
+                }
+            }
+        }
+    }
+}
 
+@Preview
+@Composable
+fun RadioButtonExample1() {
+    var state by remember { mutableStateOf(true) }
+
+    Row(Modifier.selectableGroup()) {
+        RadioButton(
+            selected = state,
+            onClick = { state = true },
+            modifier = Modifier.semantics { contentDescription = "Localized Description" }
+        )
+        RadioButton(
+            selected = !state,
+            onClick = { state = false },
+            modifier = Modifier.semantics { contentDescription = "Localized Description" }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun RadioButtonExample2() {
+    val radioOptions = listOf("Text 1", "Text 2", "Text 3")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
+// Note that Modifier.selectableGroup() is essential to ensure correct accessibility behavior
+    Column(Modifier.selectableGroup()) {
+        radioOptions.forEach { text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = { onOptionSelected(text) },
+                        role = Role.RadioButton
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = null // null recommended for accessibility with screenreaders
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
             }
         }
     }

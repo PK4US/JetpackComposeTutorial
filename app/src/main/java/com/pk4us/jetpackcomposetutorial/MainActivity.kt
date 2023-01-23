541e54f95d714b744118f287fbe297e5a915ef8c
PK4US <Stepanchuk.ei@gmail.com>
2023-01-23 16:18:47 +0200

    20 Меню

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,17 +3,231 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
-import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.PopupProperties
 
-@ExperimentalMaterial3Api
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Material3AppTheme {
-
+            Column(Modifier.verticalScroll(rememberScrollState())) {
+                MenuExample3()
             }
         }
+    }
+}
+
+
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuExample1() {
+    var expanded by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = { Text(text = "TopAppBar") },
+        actions = {
+
+            IconButton(onClick = {
+                expanded = true
+            }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Open Options"
+                )
+            }
+
+            DropdownMenu(
+                modifier = Modifier.width(width = 150.dp),
+                expanded = expanded,
+                onDismissRequest = {
+                    expanded = false
+                },
+                properties = PopupProperties()
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Edit") },
+                    onClick = { expanded = false },
+                    leadingIcon = {
+                        Icon(
+                            Icons.Outlined.Edit,
+                            contentDescription = null
+                        )
+                    })
+                DropdownMenuItem(
+                    text = { Text("Settings") },
+                    onClick = { expanded = false },
+                    leadingIcon = {
+                        Icon(
+                            Icons.Outlined.Settings,
+                            contentDescription = null
+                        )
+                    })
+                Divider()
+                DropdownMenuItem(
+                    text = { Text("Send") },
+                    onClick = { expanded = false },
+                    leadingIcon = {
+                        Icon(
+                            Icons.Outlined.Email,
+                            contentDescription = null
+                        )
+                    },
+                    trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
+            }
+        }
+    )
+}
+
+
+@Preview
+@Composable
+fun MenuExample2() {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.TopStart)
+    ) {
+        IconButton(onClick = { expanded = true }) {
+            Icon(Icons.Default.MoreVert, contentDescription = "Localized description")
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("Edit") },
+                onClick = { /* Handle edit! */ },
+                leadingIcon = {
+                    Icon(
+                        Icons.Outlined.Edit,
+                        contentDescription = null
+                    )
+                })
+            DropdownMenuItem(
+                text = { Text("Settings") },
+                onClick = { /* Handle settings! */ },
+                leadingIcon = {
+                    Icon(
+                        Icons.Outlined.Settings,
+                        contentDescription = null
+                    )
+                })
+            Divider()
+            DropdownMenuItem(
+                text = { Text("Send Feedback") },
+                onClick = { /* Handle send feedback! */ },
+                leadingIcon = {
+                    Icon(
+                        Icons.Outlined.Email,
+                        contentDescription = null
+                    )
+                },
+                trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun MenuExample3() {
+    var expanded by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        shape = RoundedCornerShape(size = 12.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(all = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(size = 62.dp),
+                painter = painterResource(id = R.drawable.dog),
+                contentDescription = "lucy pic",
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(width = 8.dp)) // gap between image and text
+            Text(
+                text = "Hi! I'm Lucy",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium
+            )
+            Spacer(modifier = Modifier.width(width = 180.dp)) // gap between image and text
+            IconButton( onClick = {
+                expanded = true
+            }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Open Options"
+                )
+            }
+        }
+    }
+
+    DropdownMenu(
+        modifier = Modifier.width(width = 150.dp),
+        expanded = expanded,
+        onDismissRequest = {
+            expanded = false
+        },
+        properties = PopupProperties()
+    ) {
+        DropdownMenuItem(
+            text = { Text("Edit") },
+            onClick = { expanded = false },
+            leadingIcon = {
+                Icon(
+                    Icons.Outlined.Edit,
+                    contentDescription = null
+                )
+            })
+        DropdownMenuItem(
+            text = { Text("Settings") },
+            onClick = { expanded = false },
+            leadingIcon = {
+                Icon(
+                    Icons.Outlined.Settings,
+                    contentDescription = null
+                )
+            })
+        Divider()
+        DropdownMenuItem(
+            text = { Text("Send") },
+            onClick = { expanded = false },
+            leadingIcon = {
+                Icon(
+                    Icons.Outlined.Email,
+                    contentDescription = null
+                )
+            },
+            trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
     }
 }

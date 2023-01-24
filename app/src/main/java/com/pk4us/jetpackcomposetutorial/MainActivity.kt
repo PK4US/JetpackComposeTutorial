fc4116dc41b7e8961718e254b27be3dd8c9fc3d7
PK4US <Stepanchuk.ei@gmail.com>
2023-01-25 00:26:10 +0200

    23 Переключатели

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,7 +3,19 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
@@ -12,8 +24,50 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-
+                Column {
+                    SwitchExample1()
+                    SwitchExample2()
+                }
             }
         }
     }
+}
+
+
+
+@Preview
+@Composable
+fun SwitchExample1() {
+    var checked by remember { mutableStateOf(true) }
+    Switch(
+        modifier = Modifier.semantics { contentDescription = "Demo" },
+        checked = checked,
+        onCheckedChange = { checked = it })
+}
+
+
+
+@Preview
+@Composable
+fun SwitchExample2() {
+    var checked by remember { mutableStateOf(true) }
+// Icon isn't focusable, no need for content description
+    val icon: (@Composable () -> Unit)? = if (checked) {
+        {
+            Icon(
+                imageVector = Icons.Filled.Check,
+                contentDescription = null,
+                modifier = Modifier.size(SwitchDefaults.IconSize),
+            )
+        }
+    } else {
+        null
+    }
+
+    Switch(
+        modifier = Modifier.semantics { contentDescription = "Demo with icon" },
+        checked = checked,
+        onCheckedChange = { checked = it },
+        thumbContent = icon
+    )
 }

fe12a52f9d8f7446deb0eb1a90d168ef8dbaad7c
PK4US <stepanchuk.ei@gmail.com>
2023-03-02 00:07:43 +0200

    BasicTextField

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,24 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
@@ -18,14 +29,135 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-               Row(
-                   Modifier.fillMaxSize(),
-                   horizontalArrangement = Arrangement.Center,
-                   verticalAlignment = Alignment.CenterVertically,
-                   ) {
-                   Text(text = "HelloWorld!")
-               }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        MyUI1()
+                        MyUI2()
+                        MyUI3()
+                        MyUI4()
+                    }
+                }
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun MyUI1() {
+    var value by remember { mutableStateOf("Пример базового текстового поля") }
+
+    BasicTextField(
+        value = value,
+        onValueChange = { newText ->
+            value = newText
+        },
+        modifier = Modifier.background(Color.Gray)
+    )
+}
+
+@Preview
+@Composable
+private fun MyUI2() {
+    var value by remember { mutableStateOf("Цвет курсора") }
+
+    BasicTextField(
+        value = value,
+        onValueChange = { newText ->
+            value = newText
+        },
+        textStyle = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.DarkGray
+        ),
+        modifier = Modifier.background(Color.Gray),
+        cursorBrush = Brush.verticalGradient(colors = listOf(Color(0xFF2193b0), Color(0xFF6dd5ed)))
+    )
+}
+
+@Preview
+@Composable
+private fun MyUI3() {
+    var value by remember { mutableStateOf("text") }
+
+    BasicTextField(
+        value = value,
+        onValueChange = { newText ->
+            value = newText
+        },
+        textStyle = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.DarkGray
+        ),
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 64.dp) // margin left and right
+                    .fillMaxWidth()
+                    .background(color = Color(0xFF20E6DF), shape = RoundedCornerShape(size = 16.dp))
+                    .border(
+                        width = 2.dp,
+                        color = Color(0xFF147772),
+                        shape = RoundedCornerShape(size = 16.dp)
+                    )
+                    .padding(all = 16.dp), // inner padding
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Favorite icon",
+                    tint = Color.DarkGray
+                )
+                Spacer(modifier = Modifier.width(width = 8.dp))
+                innerTextField()
+            }
+        }
+    )
+}
+
+
+@Preview
+@Composable
+private fun MyUI4(placeholder: String = "Enter Your Name") {
+    var value by remember { mutableStateOf("") }
+
+    BasicTextField(
+        value = value,
+        onValueChange = { newText ->
+            value = newText
+        },
+        textStyle = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.DarkGray
+        ),
+        decorationBox = { innerTextField ->
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 64.dp) // margin left and right
+                    .fillMaxWidth()
+                    .border(
+                        width = 2.dp,
+                        color = Color(0xFFAAE9E6),
+                        shape = RoundedCornerShape(size = 16.dp)
+                    )
+                    .padding(horizontal = 16.dp, vertical = 12.dp), // inner padding
+            ) {
+                if (value.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = Color.LightGray
+                    )
+                }
+                innerTextField()
+            }
+        }
+    )
 }

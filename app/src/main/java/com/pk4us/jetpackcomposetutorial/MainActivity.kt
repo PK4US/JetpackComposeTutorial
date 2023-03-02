9baec06e1f8f38115f5809328a0beaac1d41e99b
PK4US <stepanchuk.ei@gmail.com>
2023-03-02 14:38:28 +0200

    icon

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,21 @@ package com.pk4us.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.rounded.Verified
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
@@ -18,14 +26,68 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-               Row(
+                Column(
                    Modifier.fillMaxSize(),
-                   horizontalArrangement = Arrangement.Center,
-                   verticalAlignment = Alignment.CenterVertically,
+                   verticalArrangement = Arrangement.Center,
+                   horizontalAlignment = Alignment.CenterHorizontally,
                    ) {
-                   Text(text = "HelloWorld!")
+                   MyUI1()
+                   MyUI2()
+                   MyUI3()
                }
             }
         }
     }
+}
+
+
+
+@Preview
+@Composable
+fun MyUI1() {
+    Icon(
+        modifier = Modifier.size(size = 120.dp),
+        imageVector = Icons.Default.Person,
+        contentDescription = "Person Icon",
+        tint = Color.LightGray
+    )
+}
+
+
+
+@Preview
+@Composable
+fun MyUI2() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Verified,
+            contentDescription = null,
+            tint = Color.Green,
+            modifier = Modifier.size(28.dp)
+        )
+
+        // gap between icon and text
+        Spacer(modifier = Modifier.width(width = 6.dp))
+
+        Text(
+            text = "Проверено",
+            color = Color.Green,
+            fontSize = 20.sp
+        )
+    }
+}
+
+
+
+@Preview
+@Composable
+fun MyUI3() {
+    Icon(
+        painter = painterResource(id = R.drawable.baseline_fingerprint_24),
+        contentDescription = null,
+        tint = Color.Green,
+        modifier = Modifier.size(size = 60.dp)
+    )
 }

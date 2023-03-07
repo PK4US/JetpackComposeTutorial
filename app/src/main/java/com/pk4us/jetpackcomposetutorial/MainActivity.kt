d24e7aefc50df0c85ade68b0545b5708c0c299a8
PK4US <stepanchuk.ei@gmail.com>
2023-03-07 16:06:28 +0200

    Bottom Sheet (material 2)

--- app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/MainActivity.kt
@@ -1,31 +1,187 @@
 package com.pk4us.jetpackcomposetutorial
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Navigation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.pk4us.jetpackcomposetutorial.ui.theme.Material3AppTheme
+import kotlinx.coroutines.launch
 
-@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-               Row(
-                   Modifier.fillMaxSize(),
-                   horizontalArrangement = Arrangement.Center,
-                   verticalAlignment = Alignment.CenterVertically,
-                   ) {
-                   Text(text = "HelloWorld!")
-               }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+//                        MyUI1()
+                        MyUI2()
+                    }
+                }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MyUI1() {
+    val contextForToast = LocalContext.current.applicationContext
+
+    val bottomSheetState =
+        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+
+    val coroutineScope = rememberCoroutineScope()
+
+    ModalBottomSheetLayout(
+        sheetContent = {
+
+            LazyColumn {
+
+                items(count = 5) {
+
+                    ListItem(
+                        modifier = Modifier.clickable {
+
+                            Toast.makeText(contextForToast, "Item $it", Toast.LENGTH_SHORT)
+                                .show()
+
+                            coroutineScope.launch {
+                                bottomSheetState.hide()
+                            }
+                        },
+                        text = {
+                            Text(text = "Item $it")
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Favorite,
+                                contentDescription = "Favorite"
+                            )
+                        }
+                    )
+                }
+            }
+        },
+        sheetState = bottomSheetState
+    ) {
+        // app UI
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    bottomSheetState.show()
+                }
+            }) {
+                Text(text = "Show Bottom Sheet")
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MyUI2() {
+    val contextForToast = LocalContext.current.applicationContext
+
+    val coroutineScope = rememberCoroutineScope()
+
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 56.dp,
+        sheetContent = {
+
+            LazyColumn {
+
+                // the first item that is visible
+                item {
+                    Box(
+                        modifier = Modifier
+                            .height(56.dp)
+                            .fillMaxWidth()
+                            .background(color = MaterialTheme.colorScheme.primary)
+                    ) {
+                        Text(
+                            text = "Swipe up to Expand the sheet",
+                            modifier = Modifier.align(alignment = Alignment.Center),
+                            color = Color.White
+                        )
+                    }
+                }
+
+                // remaining items
+                items(count = 5) {
+                    ListItem(
+                        modifier = Modifier.clickable {
+
+                            Toast.makeText(contextForToast, "Item $it", Toast.LENGTH_SHORT)
+                                .show()
+
+                            coroutineScope.launch {
+                                scaffoldState.bottomSheetState.collapse()
+                            }
+                        },
+                        text = {
+                            Text(text = "Item $it")
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Favorite,
+                                contentDescription = "Favorite",
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    )
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                Toast.makeText(contextForToast, "FAB Click", Toast.LENGTH_SHORT).show()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Navigation,
+                    contentDescription = "Favorite"
+                )
+            }
+        }) {
+        // app UI
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Rest of the app UI")
         }
     }
 }

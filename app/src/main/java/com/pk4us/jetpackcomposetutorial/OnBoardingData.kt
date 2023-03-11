00d2320316d5d3b602faed631118e994ab043001
PK4US <Stepanchuk.ei@gmail.com>
2023-03-11 16:55:07 +0200

    бортовой экран с анимацией

---
+++ app/src/main/java/com/pk4us/jetpackcomposetutorial/OnBoardingData.kt
@@ -0,0 +1,22 @@
+package com.pk4us.jetpackcomposetutorial
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.*
+
+data class OnBoardingData(val image: Int, val title: String, val desc: String)
+
+
+
+@Composable
+fun LoaderIntro(modifier: Modifier, image: Int) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(image))
+    val progress by animateLottieCompositionAsState(composition)
+    LottieAnimation(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        //progress = { progress },
+        modifier = modifier
+    )
+}
